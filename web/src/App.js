ecc6a862a6d66a4263f8c4f1cb8b36e0b865f925
Matheus Pereira <mmontalvaop@gmail.com>
2020-01-15 08:51:09 -0300

    classe three - starting
    Início da terceira aula, onde começa o desenvolvimento da aplicação web com React

---
+++ web/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+
+  	return (
+  		<div id="app">
+  			<aside>
+  				
+  			</aside>
+  			<main>
+  			
+  			</main>
+  		</div>
+  	);
+}
+
+export default App;
+
+// Componente     	Bloco HTML isolado
+// Propriedade    	Informações que um componente pai passa para o componente filho
+// Estado  			Informações mantidas pelo componente (imutabilidade)
